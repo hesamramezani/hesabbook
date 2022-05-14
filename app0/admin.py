5deb44c74898cda9eca11cb0f10ad9856f50dae6
hesam <hesamramezani2121@gmail.com>
2022-05-14 05:21:28 -0400

    install DRF

--- app0/admin.py
+++ app0/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Expend , Income
 
-# Register your models here.
+admin.site.register(Expend)
+admin.site.register(Income)
+
--- app0/models.py
+++ app0/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Expend(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    def __str__(self):
+        return self.text
+
+
+class Income(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+
