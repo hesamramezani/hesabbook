f8dcb2f14bee6e637a04f21a83335adf9a82050e
hesam <hesamramezani2121@gmail.com>
2022-05-17 07:51:02 -0400

    sum of income and expend

--- app0/urls.py
+++ app0/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from app0.views import Expend_views , Income_views
+from app0.views import Expend_views , Income_views , Sum_of_Expend , Sum_of_Income
 
 urlpatterns = [
     path("expend/" , Expend_views.as_view() , name = "expend"),
     path("income/" , Income_views.as_view() , name = "income"),
+    path("sumofexpend/<int:pk>/" , Sum_of_Expend.as_view() , name = "Sum_of_Expend"),
+    path("sumofincome/<int:pk>/", Sum_of_Income.as_view(), name="Sum_of_Income"),
 
 ]
--- app0/views.py
+++ app0/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .serializer import Expend_serializer , Income_serializer
 from .models import Expend , Income
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from django.db.models import Sum, Count
+
 
 class Expend_views(CreateAPIView):
     queryset = Expend.objects.all()
@@ -11,3 +16,16 @@ class Expend_views(CreateAPIView):
 class Income_views(CreateAPIView):
     queryset = Income.objects.all()
     serializer_class = Income_serializer
+
+
+class Sum_of_Expend(APIView):
+    def get(self , request , pk):
+        queyset = User.objects.get(pk = pk)
+        return Response(Expend.objects.filter(user=queyset).aggregate(Count("amount") , Sum("amount")))
+
+
+
+class Sum_of_Income(APIView):
+    def get(self , request , pk):
+        queyset = User.objects.get(pk = pk)
+        return Response(Income.objects.filter(user=queyset).aggregate(Count("amount") , Sum("amount")))
