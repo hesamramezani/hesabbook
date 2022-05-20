13c48f60ff9fa233ac8d853cfc46d60672e1f987
hesam <hesamramezani2121@gmail.com>
2022-05-20 07:20:17 -0400

    create custom permission

---
+++ app0/permission.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+from django.contrib.auth.models import User
+
+
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
+
+
+
+class IsOwner(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user == User.objects.get(pk=view.kwargs['pk']))
+
